0742da751efc1c4e743669a404efe82d9e2dba68
Evgeniya Kozhevnikova <mosikhinaS@yandex.ru>
2026-09-25 13:12:01 +0400

    feat: add test ingrigients

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -56,3 +56,25 @@ class TestBurger:
         burger.set_buns(bun2)
         assert burger.bun.get_name() == "Black Bun"
         assert burger.bun.get_price() == 100
+
+    def test_add_ingredient(self):
+        "Проверка добавления ингредиента"
+        burger = Burger()
+        bun = Bun("white bun", 200)
+        burger.set_buns(bun)
+        ingredient = Ingredient("FILLING", "dinosaur", 200)
+        burger.add_ingredient(ingredient)
+        assert len(burger.ingredients) == 1
+        assert burger.ingredients[0] is ingredient
+        assert burger.ingredients[0].get_name() == "dinosaur"
+
+    def test_add_ingredient_with_mock(self):
+        "Проверка добавления ингредиента с моком"
+        burger = Burger()
+        mock_ingredient = Mock(spec=Ingredient)
+        mock_ingredient.get_type.return_value = "FILLING"
+        mock_ingredient.get_name.return_value = "Mock Ingredient"
+        mock_ingredient.get_price.return_value = 275
+        burger.add_ingredient(mock_ingredient)
+        assert len(burger.ingredients) == 1
+        assert burger.ingredients[0] is mock_ingredient
